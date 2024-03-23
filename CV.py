c5a57509a322153e6c4b08999f98788ba808b3ce
73XIS <162714525+73XIS@users.noreply.github.com>
2024-03-23 11:35:31 +0200

    Update CV.py

--- CV.py
+++ CV.py
@@ -34,9 +34,6 @@ with st.container():
 			st.caption("Location: Cluj-Napoca, Cluj")
 			st.markdown(
 				"""
-    				2024\n
-				-  ~ website made from scratch by me for my father's company\n
-				February 2023 - June 2023\n
 				- **Barista at *Meron***\n
 				October 2019 - November 2023\n
 				- **Volunteer at *Societatea Studentilor Europenisti***\n
